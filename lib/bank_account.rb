95e34076996fe795db9a05e501ec38420e8e6c41
JohnDaise <jdaise@gmail.com>
2018-06-28 17:42:37 -0400

    final commit

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,44 @@
+require 'pry'
+require "/Users/JohnDaise/Desktop/Flatiron/oo-banking-dc-web-062518/lib/bank_account.rb"
+
 class BankAccount
+
+attr_accessor :balance, :status
+attr_reader :name
+
+
+def initialize(name)
+  @name = name
+  @balance = 1000
+  @status = "open"
+end
+
+def deposit(amount)
+  @balance += amount
+end
+
+def display_balance
+  puts self
+  "Your balance is $#{balance}."
+end
+
+def valid?
+  if @status == "open" && @balance > 0
+    true
+  else
+    false
+  end
+end
+
+
+def close_account
+  self.status = "closed"
+end
+
+
+
+
+
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,49 @@
+require 'pry'
+require "/Users/JohnDaise/Desktop/Flatiron/oo-banking-dc-web-062518/lib/bank_account.rb"
+
 class Transfer
   # your code here
+attr_accessor :sender, :receiver, :amount, :status, :execute_transaction
+attr_reader
+
+
+
+def initialize(sender, receiver, amount)
+  @sender = sender
+  @receiver = receiver
+  @amount = amount
+  @status = 'pending'
+end
+
+
+def valid?
+   sender.valid? && receiver.valid?
+end
+
+def execute_transaction
+    @execute_transaction ||= begin
+    if sender.balance >= self.amount
+      sender.balance -= self.amount
+      receiver.balance += self.amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+end
+
+def reverse_transfer
+   #binding.pry
+   if @execute_transaction == "complete"
+    sender.balance += self.amount
+    receiver.balance -= self.amount
+    @status = "reversed"
+   end
+
+end
+
+
+
+
 end
